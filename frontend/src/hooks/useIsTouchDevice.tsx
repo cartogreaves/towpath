94b3e541393299f2e34aa62cd317a8a06aaa0c67
cartogreaves <richard.hargreaves@live.co.uk>
2026-03-21 20:07:58 +0000

    Empty repo

--- frontend/src/hooks/useIsTouchDevice.tsx
+++
@@ -1,6 +0,0 @@
-export function useIsTouchDevice() {
-    return (('ontouchstart' in window) ||
-      (navigator.maxTouchPoints > 0) ||
-      // @ts-ignore
-      (navigator.msMaxTouchPoints > 0));
-  }
